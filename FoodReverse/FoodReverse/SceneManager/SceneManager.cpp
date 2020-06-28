88f9ab914217b8008f001c36f54dc6081dfc1a59
mono1140 <11potato30@gmail.com>
2020-06-28 22:08:02 +0900

    :sparkles: SceneManager の追加

---
+++ FoodReverse/FoodReverse/SceneManager/SceneManager.cpp
@@ -0,0 +1,68 @@
+﻿#include "SceneManager.h"
+
+#include "../Scene/Title/Title.h"
+#include "../Scene/ModeSelect/ModeSelect.h"
+#include "../Scene/SkinSelect/SkinSelect.h"
+#include "../Scene/Game/GameScene.h"
+#include "../Scene/Result/Result.h"
+
+void SceneManager::CreateScene(SceneName name)
+{
+	delete scene;
+	scene = nullptr;
+
+	switch (name)
+	{
+	case SceneName::TitleScene:
+		scene = new TitleScene();
+		break;
+	case SceneName::ModeSelectScene:
+		scene = new ModeSelectScene();
+		break;
+	case SceneName::SkinSelectScene:
+		scene = new SkinSelectScene();
+		break;
+	case SceneName::GameScene:
+		scene = new GameScene();
+		break;
+	case SceneName::ResultScene:
+		scene = new ResultScene();
+		break;
+	default:
+		break;
+	}
+
+}
+
+void SceneManager::SceneUpdate()
+{
+	scene->Update();
+}
+
+void SceneManager::SceneDrow()
+{
+	scene->Draw();
+}
+
+bool SceneManager::IsSceneChange()
+{
+	// Exitで現在のSceneと同じ名前が返ってくれば false
+	if (scene->Exit() == currentScene)
+	{
+		return false;
+	}
+
+	// 違うなら返ってきた名前のSceneにChange
+	return true;
+}
+
+void SceneManager::Initialize(SceneName name)
+{
+	CreateScene(name);
+}
+
+void SceneManager::Delete()
+{
+	delete scene;
+	scene = nullptr;
+}
---
+++ FoodReverse/FoodReverse/SceneManager/SceneManager.h
@@ -0,0 +1,35 @@
+﻿#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+
+#include "../Scene/SceneBase.h"
+
+class SceneManager
+{
+public:
+
+	SceneManager(SceneName name) { Initialize(name); }
+
+	void CreateScene(SceneName name);
+
+	void SceneUpdate();
+
+	void SceneDrow();
+
+	bool IsSceneChange();
+
+private:
+
+	void Initialize(SceneName name);
+
+	void Delete();
+
+	SceneBase* scene = nullptr;
+
+	SceneName currentScene = SceneName::TitleScene;
+
+
+
+
+};
+
+#endif
