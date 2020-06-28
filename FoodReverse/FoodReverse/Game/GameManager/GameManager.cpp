b576758ed2384d7ff4a706b28ebafb12b5a27323
mono1140 <11potato30@gmail.com>
2020-06-28 23:44:55 +0900

    :sparkles: [ #6 ] GameManagerClassの作成

---
+++ FoodReverse/FoodReverse/Game/GameManager/GameManager.cpp
@@ -0,0 +1,17 @@
+#include "GameManager.h"
+
+void GameManager::Update()
+{
+}
+
+void GameManager::Drow()
+{
+}
+
+void GameManager::Initialize()
+{
+}
+
+void GameManager::Delete()
+{
+}
---
+++ FoodReverse/FoodReverse/Game/GameManager/GameManager.h
@@ -0,0 +1,38 @@
+﻿#ifndef GMAE_MANAGER_H
+#define GMAE_MANAGER_H
+
+class GameManager
+{
+public:
+
+	GameManager() { Initialize(); }
+	~GameManager() { Delete(); }
+
+	/**
+	 * Update関数をここで呼ぶ
+	 */
+	void Update();
+
+	/**
+	 * Draw関数をここで呼ぶ
+	 */
+	void Drow();
+
+private:
+
+	/**
+	 * 初期化関数
+	 */
+	void Initialize();
+
+
+	/**
+	 * 削除関数
+	 * @detail デストラクタで呼び、delete処理をメインで行う
+	 */
+	void Delete();
+
+};
+
+
+#endif
