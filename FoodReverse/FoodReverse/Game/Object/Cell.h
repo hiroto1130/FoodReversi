910ce2bc46908c08b88a6657be615cb11305c403
mono1140 <11potato30@gmail.com>
2020-06-30 23:06:39 +0900

    :sparkles: [ #8 ]  CellClassの作成

---
+++ FoodReverse/FoodReverse/Game/Object/Cell.h
@@ -0,0 +1,20 @@
+﻿#ifndef NEW_H
+#define NEW_H
+
+#include "../../Utility/CellState.h"
+
+class Cell
+{
+public:
+
+	CellState GetState() { return state; };
+
+	void SetState(CellState chang_state) { state = chang_state; };
+
+private:
+
+	CellState state = CellState::Invisible;
+
+};
+
+#endif
