9304663fd8428119a0c87aca6b89043314380252
mono1140 <11potato30@gmail.com>
2020-07-01 23:50:54 +0900

    :art: [ #8 ]  裏返すことのできる方向を保持しておく為の配列を作成
    それについてのgetter,setterを作成

--- FoodReverse/FoodReverse/Game/Object/Cell.h
+++ FoodReverse/FoodReverse/Game/Object/Cell.h
@@ -1,7 +1,10 @@
-﻿#ifndef NEW_H
-#define NEW_H
+﻿#ifndef CELL_H
+#define CELL_H
+
+#include <vector>
 
 #include "../../Utility/CellState.h"
+#include "../../Utility/Direction.h"
 
 class Cell
 {
@@ -11,9 +14,15 @@ public:
 
 	void SetState(CellState chang_state) { state = chang_state; };
 
+	std::vector<Direction> GetCanInvertDirection() { return can_invert_direction; }
+
+	void SetCanInvertDirection(std::vector<Direction> direction) { can_invert_direction = direction; }
+
 private:
 
 	CellState state = CellState::Invisible;
+
+	std::vector<Direction> can_invert_direction;
 
 };
 
