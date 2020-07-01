904d215b9c8ac372da2b94b25bfaa234332c5482
mono1140 <11potato30@gmail.com>
2020-07-01 23:53:00 +0900

    :sparkles: [ #6 ]  GameSystemClassの作成

---
+++ FoodReverse/FoodReverse/Game/System/GameSystem.h
@@ -0,0 +1,21 @@
+﻿#ifndef GAME_SYSTEM_H
+#define GAME_SYSTEM_H
+
+#include "../Object/Board.h"
+
+class GameSystem
+{
+public:
+
+	void Invert(Board board);
+
+	void Search();
+
+	void Support();
+
+private:
+
+
+};
+
+#endif
