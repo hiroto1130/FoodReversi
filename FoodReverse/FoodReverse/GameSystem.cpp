6c6e3be0cedb7269e5273436c47285fb4aa4dbae
mono1140 <11potato30@gmail.com>
2020-07-06 01:35:26 +0900

    :sparkles: [ #7 ] 無名namespaceに各定数を作成

--- FoodReverse/FoodReverse/GameSystem.cpp
+++ FoodReverse/FoodReverse/GameSystem.cpp
@@ -1,7 +1,22 @@
 ﻿#include "Game\System\GameSystem.h"
 
 void GameSystem::Invert()
+namespace
 {
+	const int UP = 0;
+	const int DOWN = 1;
+	const int RIGHT = 2;
+	const int LEFT = 3;
+	const int UP_RIGHT = 4;
+	const int UP_LEFT = 5;
+	const int DOWN_RIGHT = 6;
+	const int DOWN_LEFT = 7;
+	const int ADD_X = 0;
+	const int ADD_Y = 1;
+
+	const int MAX_CELL = 64;
+	const int MIN_CELL = 0;
+}
 }
 
 void GameSystem::Search()
