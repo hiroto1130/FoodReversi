b6ad9ed565177f4bac7f544e62905730e5bdc7ab
mono1140 <11potato30@gmail.com>
2020-07-06 00:28:21 +0900

    :art: [ #8 ] SetterでSetする際に配列を初期化する処理を追加

--- FoodReverse/FoodReverse/Game/Object/Cell.h
+++ FoodReverse/FoodReverse/Game/Object/Cell.h
@@ -16,8 +16,11 @@ public:
 
 	std::vector<int> GetCanInvertDirection() { return can_invert_direction; }
 
-	void SetCanInvertDirection(std::vector<int> direction) { can_invert_direction = direction; }
-
+	void SetCanInvertDirection(std::vector<int> direction)
+	{
+		can_invert_direction.clear();
+		can_invert_direction = direction;
+	}
 private:
 
 	CellState state = CellState::Invisible;
